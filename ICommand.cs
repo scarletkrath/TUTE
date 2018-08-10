8efef5d3f3ff6473e02026d22fdddb8e15c72b36
Suleyth <42274829+scarletkrath@users.noreply.github.com>
2018-08-10 16:53:25 +0200

    Add files via upload

---
+++ ICommand.cs
@@ -0,0 +1,8 @@
+﻿namespace TUTE.Commands
+{
+public interface ICommand
+{
+    void ParseArgs(string[] args); // convert the data you want to use
+    void Execute(); // do whatever with those values
+}
+}
